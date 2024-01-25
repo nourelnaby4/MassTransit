d66721678a6229a957007faa91b76d25b5a94b9c
nourelnaby4 <nourelnaby4@gmail.com>
2024-01-25 23:27:14 +0200

    add RabbitMq setting in appsetting

--- Web.Api/Program.cs
+++ Web.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Options;
+using Web.Api.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<MessageBrokerSetting>(builder.Configuration.GetSection("MessageBroker"));
+builder.Services.AddSingleton(sp=>sp.GetRequiredService<IOptions<MessageBrokerSetting>>().Value);
 
 var app = builder.Build();
 
--- Web.Api/Settings/MessageBrokerSetting.cs
+++ Web.Api/Settings/MessageBrokerSetting.cs
@@ -2,5 +2,8 @@
 {
     public class MessageBrokerSetting
     {
+        public string Host { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
